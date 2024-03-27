5aafc9cd268fd394f3feb012c7b9a42a5cd1285f
Alexandr <amakutsevich@bk.ru>
2024-03-27 14:42:21 +0700

    Add checking the data in the form

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,6 +1,10 @@
 package tacos;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,13 +12,29 @@ import java.util.List;
 @Data
 public class TacoOrder {
 
+    @NotBlank(message = "Требуется наименование доставки")
     private String deliveryName;
+
+    @NotBlank(message = "Требуется улица")
     private String deliveryStreet;
+
+    @NotBlank(message = "Требуется город")
     private String deliveryCity;
+
+    @NotBlank(message = "Укажите адрес")
     private String deliveryState;
+
+    @NotBlank(message = "Укажите почтовый индекс")
     private String deliveryZip;
+
+    @CreditCardNumber(message="Недействительный номер кредитной карты")
     private String ccNumber;
+
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
+            message="Должно быть отформатировано ММ/ГГ")
     private String ccExpiration;
+
+    @Digits(integer=3, fraction=0, message="Недействительный CVV")
     private String ccCVV;
 
     private List<Taco> tacos = new ArrayList<>();
--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -1,6 +1,8 @@
 package tacos.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,10 +23,16 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order,
+    public String processOrder(@Valid TacoOrder order, Errors errors,
                                SessionStatus sessionStatus) {
+
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
+
         return "redirect:/";
     }
 }
